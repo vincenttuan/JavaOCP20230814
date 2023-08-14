fa6ca2c3137148f268ddc19207845698f54936fc
student <student@BE-403->
2023-08-14 19:55:44 +0800

    java

--- src/day01/MyATM.java
+++ src/day01/MyATM.java
@@ -28,6 +28,23 @@ public class MyATM {
 		// 系統主畫面服務迴圈
 		while (true) {
 			System.out.print("主選單操作:\n1.查詢餘額\n2.存款\n3.提款\n4.退出\n==> ");
+            int itemId = sc.nextInt(); // 得到使用者所輸入的選單選項
+            switch (itemId) {
+				case 1: // 查詢餘額
+					System.out.printf("目前餘額 $%d\n", sysBalance);
+					break;
+				case 2: // 存款
+					break;
+				case 3: // 提款
+					break;
+				case 4: // 退出
+					System.out.println("謝謝您的使用, 歡迎下次再度光臨");
+					System.exit(0); // 強制離該系統(0:正常狀態, 1:異常狀態)
+					break;
+				default: 
+					System.out.println("選項輸入錯誤請重新輸入!");
+					break;
+			}
             
 		}
 		
