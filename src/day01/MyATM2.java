5f155bf971c77f64427b4a8e319e7ac40018f963
student <student@BE-403->
2023-08-14 20:55:14 +0800

    java

--- src/day01/MyATM2.java
+++ src/day01/MyATM2.java
@@ -15,12 +15,33 @@ public class MyATM2 {
 	
 	// 1. 驗證密碼
 	public static void checkPin() {  
-		
+		// 密碼驗證服務迴圈
+		while (true) {
+			// 1.請輸入密碼
+			System.out.print("請輸入密碼:");
+			int userPin = sc.nextInt(); // 得到使用者所輸入的密碼
+			
+			// 2.密碼驗證
+			if(userPin != sysPin) {
+				System.out.println("密碼輸入錯誤, 請重新輸入!");
+				continue; // 重跑回圈
+			} else {
+				System.out.println("密碼驗證成功!");
+				break; // 跳離迴圈
+			}
+		}
+		// 呼叫 showMenu
+		showMenu();
 	} 
 	
 	// 2. 主畫面選單
 	public static void showMenu() {  
-		
+		// 系統主畫面服務迴圈
+		while (true) {
+			System.out.print("主選單操作:\n1.查詢餘額\n2.存款\n3.提款\n4.退出\n==> ");
+            int itemId = sc.nextInt(); // 得到使用者所輸入的選單選項
+            
+		}
 	}
 	
 	// 2.1 餘額查詢
