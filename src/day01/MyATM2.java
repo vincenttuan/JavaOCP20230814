5ea89e02e75b05d4d1e313df52c8c3dbf4cddd2f
student <student@BE-403->
2023-08-14 20:50:56 +0800

    java

---
+++ src/day01/MyATM2.java
@@ -0,0 +1,41 @@
+package day01;
+
+// 根據 MyATM 進行優化
+public class MyATM2 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	// 1. 驗證密碼
+	public static void checkPin() {  
+		
+	} 
+	
+	// 2. 主畫面選單
+	public static void showMenu() {  
+		
+	}
+	
+	// 2.1 餘額查詢
+	public static void checkBalance() {  
+		
+	}
+	
+	// 2.2 存款
+	public static void deposit() {  
+		
+	}
+	
+	// 2.3 提款
+	public static void withdraw() {  
+		
+	}
+	
+	// 2.4 離開
+	public static void quit() {  
+		
+	}	
+	
+	
+}
