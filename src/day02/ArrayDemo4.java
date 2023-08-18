3d697e371593d047ac8813ca4d6ea85e15f960ca
student <student@BE-403->
2023-08-18 21:18:28 +0800

    java

--- src/day02/ArrayDemo4.java
+++ src/day02/ArrayDemo4.java
@@ -9,8 +9,57 @@ package day02;
 public class ArrayDemo4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double[] heights = {172, 168, 164, 170, 176};
+		double[] weights = {62, 57, 58, 64, 64};
+		
+		double heightAvg = getAverage(heights);
+		System.out.printf("身高的平均: %.1f\n", heightAvg);
+		double weightAvg = getAverage(weights);
+		System.out.printf("體重的平均: %.1f\n", weightAvg);
+		
+		double heightSD = getSD(heights, heightAvg);
+		System.out.printf("身高的標準差: %.3f\n", heightSD);
+		double weightSD = getSD(weights, weightAvg);
+		System.out.printf("體重的標準差: %.3f\n", weightSD);
+		
+		double heightCV = getCV(heightSD, heightAvg);
+		System.out.printf("身高的變異係數: %.3f\n", heightCV);
+		double weightCV = getCV(weightSD, weightAvg);
+		System.out.printf("體重的變異係數: %.3f\n", weightCV);
+		
+		
 	}
+	
+	// 計算變異係數 CV
+	public static double getCV(double sd, double avg) {
+		double cv = sd / avg;
+		return cv;
+	}
+	
+	// 計算標準差 SD
+	public static double getSD(double[] values, double avg) {
+		int n = values.length;
+		double sd = 0;
+		double powerSum = 0; // 平方和
+		for(double x : values) {
+			powerSum += Math.pow(x - avg, 2);
+		}
+		sd = Math.sqrt(powerSum / n);
+		return sd;
+	}
+	
+	// 建立一個可以計算平均的方法
+	// 方法參數: values 是一個一維的 double[] 陣列
+	// 方法回傳值: double, 指的就是計算平均的結果
+	public static double getAverage(double[] values) {
+		double sum = 0;
+		for(double x : values) {
+			sum += x;
+		}
+		double avg = sum / values.length;
+		return avg;
+	}
+	
+	
 
 }
