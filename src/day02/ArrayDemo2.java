879d0c9c5fcb4793cb6cf6c96347667d772ad130
student <student@BE-403->
2023-08-18 19:40:15 +0800

    java

--- src/day02/ArrayDemo2.java
+++ src/day02/ArrayDemo2.java
@@ -5,6 +5,13 @@ public class ArrayDemo2 {
 		// 學員成績資料(合法成績: 0~100)
 		int[] scores = {78, 53, -30, 210, 95, 83, 47};
 		// 請印出合法成績
+		for(int x : scores) {
+			if(x >= 0 && x <= 100) {
+				System.out.print(x + ", ");
+			}
+		}
+		System.out.println();
+		// 請幫我算出合法成績的總分與平均 = ?
 		
 		
 	}
