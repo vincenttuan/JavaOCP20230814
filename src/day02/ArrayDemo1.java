2b0c8dc32e980ac83bbfb44439a471c2ca4f4034
student <student@BE-403->
2023-08-18 19:26:00 +0800

    java

--- src/day02/ArrayDemo1.java
+++ src/day02/ArrayDemo1.java
@@ -15,7 +15,7 @@ public class ArrayDemo1 {
 		System.out.print(foo[3] + ", ");
 		System.out.println(foo[4]);
 		
-		// 利用 for-loop 遍歷陣列, 並將每一個元素依序印出
+		// 1. 利用 for-loop 遍歷陣列, 並將每一個元素依序印出
 		// 陣列的最小維度 = 0
 		// 陣列的最大維度 = 陣列的長度 - 1
 		for(int i=0;i<foo.length;i++) {
@@ -23,7 +23,14 @@ public class ArrayDemo1 {
 		}
 		System.out.println(); // 換行
 		
-
+		// 2. 利用增強型 for 迴圈(for-each / for-in)
+		// 會自動遍歷每一個陣列元素, 遍歷完之後就會自動離開迴圈
+		for(int x : foo) {
+			System.out.print(x + ", ");
+		}
+		System.out.println(); // 換行
+		
+		
 	}
 
 }
