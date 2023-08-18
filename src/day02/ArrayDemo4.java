6c82dbbff2bdf8d8d8bd2d4c0f6e877d47eda827
student <student@BE-403->
2023-08-18 20:35:47 +0800

    java

---
+++ src/day02/ArrayDemo4.java
@@ -0,0 +1,16 @@
+package day02;
+
+/*
+ * 調查五位學生之身高及體重如下:
+ * 身高：172、168、164、170、176(公分)
+ * 體重：62、57、58、64、64(公斤)
+ * 求身高,體重分散程度哪一個大
+ * */
+public class ArrayDemo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
