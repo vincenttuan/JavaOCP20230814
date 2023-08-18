0fd4f7cb27b9996f95705ecbf99e3afd576e874f
student <student@BE-403->
2023-08-18 21:37:07 +0800

    java

--- src/day02/ArrayDemo5.java
+++ src/day02/ArrayDemo5.java
@@ -1,5 +1,9 @@
 package day02;
 
+import static day02.ArrayDemo4.getAverage;
+import static day02.ArrayDemo4.getSD;
+import static day02.ArrayDemo4.getCV;
+
 /*
  * 某公司有18位員工，
  * 其中10位在去年投資股票一年的獲利率如下(單位：%)：
@@ -14,6 +18,20 @@ public class ArrayDemo5 {
 		double[] stocks = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
         double[] bonds  = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2};
         
+        double stockAvg = getAverage(stocks);       // 股票獲利平均
+        double bondAvg  = getAverage(bonds);        // 債券獲利平均
+        double stockSD  = getSD(stocks, stockAvg);  // 股票標準差
+        double bondSD   = getSD(bonds, bondAvg);    // 債券標準差
+        double stockCV  = getCV(stockSD, stockAvg); // 股票變異係數
+        double bondCV   = getCV(bondSD, bondAvg);   // 債券變異係數
+        
+        System.out.printf("股票變異係數: %.3f\n", stockCV);
+        System.out.printf("債券變異係數: %.3f\n", bondCV);
+        if(stockCV < bondCV) {
+        	System.out.println("股票投資較穩健");
+        } else {
+        	System.out.println("債券投資較穩健");
+        }
 	}
 	
 	
