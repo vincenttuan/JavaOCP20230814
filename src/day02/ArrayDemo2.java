67c34a2234d7aee9c141450358af047c78281127
student <student@BE-403->
2023-08-18 19:49:40 +0800

    java

--- src/day02/ArrayDemo2.java
+++ src/day02/ArrayDemo2.java
@@ -11,8 +11,20 @@ public class ArrayDemo2 {
 			}
 		}
 		System.out.println();
-		// 請幫我算出合法成績的總分與平均 = ?
 		
+		// 請幫我算出合法成績的總分與平均 = ?
+		int sum = 0; // 計算總分用
+		int count = 0; // 合法成績的數量
+		for(int x : scores) {
+			if(x >= 0 && x <= 100) { // 合法成績: 0~100
+				sum += x; // sum = sum + x
+				count++; // 合法成績的數量 + 1
+			}
+		}
+		double avg = (count == 0) ? 0 : (double)sum / count;
+		
+		System.out.printf("合法成績總分: %d\n", sum);
+		System.out.printf("合法成績平均: %.1f\n", avg);
 		
 	}
 }
