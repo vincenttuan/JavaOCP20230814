03606cd3d82bf213be5fa9a5b8238e4354647b04
student <student@BE-403->
2023-08-18 19:52:33 +0800

    java

---
+++ src/day02/ArrayDemo3.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class ArrayDemo3 {
+
+	public static void main(String[] args) {
+		// 學員成績資料(合法成績: 0~100)
+		int[] scores = {78, 53, -30, 210, 95, 83, 47};
+		// 請求出合法成績中最高分與最低分各為多少 ?
+		
+	}
+
+}
