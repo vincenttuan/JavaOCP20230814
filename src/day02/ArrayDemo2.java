cc5f4887ce38c2483398b21ed19da584e82ecc8e
student <student@BE-403->
2023-08-18 19:38:05 +0800

    java

---
+++ src/day02/ArrayDemo2.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class ArrayDemo2 {
+	public static void main(String[] args) {
+		// 學員成績資料(合法成績: 0~100)
+		int[] scores = {78, 53, -30, 210, 95, 83, 47};
+		// 請印出合法成績
+		
+		
+	}
+}
