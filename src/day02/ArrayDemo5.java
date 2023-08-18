3bea552782e92691b59ea013dea8ae7838a89cbb
student <student@BE-403->
2023-08-18 21:29:57 +0800

    java

--- src/day02/ArrayDemo5.java
+++ src/day02/ArrayDemo5.java
@@ -11,8 +11,10 @@ package day02;
 public class ArrayDemo5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double[] stocks = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
+        double[] bonds  = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2};
 
 	}
-
+	
+	
 }
