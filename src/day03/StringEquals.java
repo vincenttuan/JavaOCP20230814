6a1299f63beceadcb8af2559eeeda2f88ab2c18e
student <student@BE-403->
2023-08-21 20:39:07 +0800

    java

--- src/day03/StringEquals.java
+++ src/day03/StringEquals.java
@@ -3,15 +3,20 @@ package day03;
 public class StringEquals {
 
 	public static void main(String[] args) {
-		String s1 = new String("Java");
+		String s1 = new String("Java"); // 配置在 String pool 外
 		char[] c1 = {'J', 'a', 'v', 'a'};
-		String s2 = new String(c1);
+		String s2 = new String(c1); // 配置在 String pool 外
+		String s3 = "Java"; // 配置在 String pool 內 
+		
 		System.out.println("s1 = " + s1);
 		System.out.println("s2 = " + s2);
+		System.out.println("s3 = " + s3);
 		System.out.println("s1 == s2 得到 " + (s1 == s2));
 		System.out.println("s1.equals(s2) 得到 " + s1.equals(s2));
 		
-		
+		System.out.println("s3 == s2 得到 " + (s3 == s2));
+		s2 = s2.intern(); // 將 s2 配置在 String pool 內, 若有相同內容的字串則共用
+		System.out.println("s3 == s2 得到 " + (s3 == s2));
 
 	}
 
