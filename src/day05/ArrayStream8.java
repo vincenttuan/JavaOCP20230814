d45deeb5afe975c792bb9b8a754c8056dc5fae86
teacher <teacher@BE-403->
2023-08-28 21:29:55 +0800

    java

--- src/day05/ArrayStream8.java
+++ src/day05/ArrayStream8.java
@@ -7,7 +7,15 @@ public class ArrayStream8 {
 		int[] yearOfExperience = {3, 8, 4, 6}; // 年資
 		double[] salaries = {60000.0, 80000.0, 65000.0, 75000.0}; // 薪資
 		
-		
+		// Java 8 以前
+		for(int i=0;i<yearOfExperience.length;i++) {
+			if(yearOfExperience[i] >= 5) {
+				// 年資大於 5 的 index
+				System.out.println(i);
+				System.out.println(names[i]);
+				System.out.println(salaries[i]);
+			}
+		}
 		
 		
 	}
