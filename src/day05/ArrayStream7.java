70c94f2f0d2ce8c318521238a9d251663205ad66
teacher <teacher@BE-403->
2023-08-28 21:13:52 +0800

    java

--- src/day05/ArrayStream7.java
+++ src/day05/ArrayStream7.java
@@ -17,7 +17,12 @@ public class ArrayStream7 {
 			  .forEach(out::println);
 		
 		// 請問平均年齡幾歲 ?
-		double avgAge = 
+		double avgAge = Arrays.stream(births)
+							  .map(birth -> birth.substring(0, 4)) // "2000", "2008", "2005"
+							  .mapToInt(birth -> Integer.parseInt(birth)) // 2000, 2008, 2005
+							  .map(birthYear -> currentYear-birthYear) // 23, 15, 18
+							  .average()
+							  .orElse(0);
 		
 		
 		
