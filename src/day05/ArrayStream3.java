6e2dad65627d90f216340c8315d099f50348e3c5
teacher <teacher@BE-403->
2023-08-28 19:30:51 +0800

    java

---
+++ src/day05/ArrayStream3.java
@@ -0,0 +1,20 @@
+package day05;
+
+import java.util.Arrays;
+import static java.lang.System.out; // 靜態 import (jdk 1.4 開始提供)
+public class ArrayStream3 {
+
+	public static void main(String[] args) {
+		// 陣列與串流 stream
+		// 陣列轉串流可以透過 Arrays.stream()
+		int[] scores = {100, 50, 40, 70, 90};
+		// 印出及格分數的總分
+		// Java 8 以前
+		
+		
+		// Java 8 以後
+		
+		
+	}
+
+}
