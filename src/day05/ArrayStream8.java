a7afac9c57f2bc535492485664c0055c353ab914
teacher <teacher@BE-403->
2023-08-28 21:24:19 +0800

    java

---
+++ src/day05/ArrayStream8.java
@@ -0,0 +1,14 @@
+package day05;
+
+public class ArrayStream8 {
+	public static void main(String[] args) {
+		// 想要知道年資超過5年有誰且平均薪資為何?
+		String[] names = {"Alice", "Bob", "Charlie", "David"}; // 姓名
+		int[] yearOfExperience = {3, 8, 4, 6}; // 年資
+		double[] salaries = {60000.0, 80000.0, 65000.0, 75000.0}; // 薪資
+		
+		
+		
+		
+	}
+}
