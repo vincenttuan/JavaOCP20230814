2a5a19f410c7c76b263c2db272b70feec051dddb
teacher <teacher@BE-403->
2023-08-28 21:13:10 +0800

    java

---
+++ src/day05/ArrayStream7.java
@@ -0,0 +1,27 @@
+package day05;
+
+import java.util.Arrays;
+import static java.lang.System.out;
+
+public class ArrayStream7 {
+
+	public static void main(String[] args) {
+		int currentYear = 2023;
+		// mapToXXX 進行練習
+		String[] births = {"2000-01-01", "2008-09-02", "2005-12-03"};
+		// 印出每一個年齡 ?
+		Arrays.stream(births)
+			  .map(birth -> birth.substring(0, 4)) // "2000", "2008", "2005"
+			  .mapToInt(birth -> Integer.parseInt(birth)) // 2000, 2008, 2005
+			  .map(birthYear -> currentYear-birthYear) // 23, 15, 18
+			  .forEach(out::println);
+		
+		// 請問平均年齡幾歲 ?
+		double avgAge = 
+		
+		
+		
+		
+	}
+
+}
