d236eb72650358e8c4199b3e3815b46c8359b43c
teacher <teacher@BE-403->
2023-09-01 21:23:25 +0800

    java

---
+++ src/day06/DrinkDemo3.java
@@ -0,0 +1,20 @@
+package day06;
+
+import java.util.Arrays;
+
+public class DrinkDemo3 {
+
+	public static void main(String[] args) {
+		Drink drink1 = new Drink("咖啡", 75, 500);
+		Drink drink2 = new Drink("冰茶", 2, 700);
+		Drink drink3 = new Drink("豆漿", 35, 600);
+		Drink drink4 = new Drink("牛奶", 50, 200);
+		Drink drink5 = new Drink("果汁", 10, 300);
+		
+		Drink[] drinks = {drink1, drink2, drink3, drink4, drink5};
+		// 請計算出冷熱飲各有多少容量
+		
+
+	}
+
+}
