87f431c952f04c483962f8b01b4df7ff351c08aa
teacher <teacher@BE-403->
2023-09-01 20:01:04 +0800

    java

--- src/day06/CarDemo5.java
+++ src/day06/CarDemo5.java
@@ -1,5 +1,7 @@
 package day06;
 
+import java.util.Arrays;
+
 public class CarDemo5 {
 
 	public static void main(String[] args) {
@@ -14,7 +16,23 @@ public class CarDemo5 {
 		// 請將黑車加速一次, 紅車減速二次
 		// 最後印出每一台車的顏色與車速
 		// 請用 Java 8 Stream 實現
-		
+		Arrays.stream(cars)
+			  .peek(car -> {  // 觀察每一個元素
+				  switch (car.color) {
+					case "黑":
+						car.accelerate(); // 加速一次
+						break;
+					case "紅":
+						// 減速二次
+						car.brake(); 
+						car.brake();
+						break;
+				  }
+			  })
+			  .forEach(car -> {
+					// 印出資訊
+					System.out.printf("%s, %d\n", car.color, car.speed);
+			  });
 			
 	}
 
