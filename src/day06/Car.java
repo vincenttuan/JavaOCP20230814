4061e2b1abb140b1ca73a0931ee2af6afdbaa138
teacher <teacher@BE-403->
2023-09-01 19:15:25 +0800

    java

--- src/day06/Car.java
+++ src/day06/Car.java
@@ -4,6 +4,17 @@ public class Car {
 	String color; // 物件屬性: 顏色
 	int speed;    // 物件屬性: 速度
 	
+	// 建構子
+	Car() {
+		System.out.println("Car 建構子1");
+	}
+	
+	Car(String c, int s) {
+		System.out.println("Car 建構子2");
+		color = c;
+		speed = s;
+	}
+	
 	void accelerate() { // 物件方法: 加速
 		speed += 5; // speed = speed + 5;
 	}
