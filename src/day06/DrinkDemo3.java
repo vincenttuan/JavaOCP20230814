53107f37d0da0d4a6ba2798db2d4e2252ccb7756
teacher <teacher@BE-403->
2023-09-01 21:33:19 +0800

    java

--- src/day06/DrinkDemo3.java
+++ src/day06/DrinkDemo3.java
@@ -14,6 +14,22 @@ public class DrinkDemo3 {
 		Drink[] drinks = {drink1, drink2, drink3, drink4, drink5};
 		// 請計算出冷熱飲各有多少容量
 		// 飲料溫度 (小於5度表示冷飲, 大於60度表示熱飲)
+		// 計算冷飲的總容量
+		int coldDrinkVolume = 0;
+		for(Drink drink : drinks) {
+			if(drink.temperature < 5) {
+				coldDrinkVolume += drink.volume;
+			}
+		}
+		System.out.printf("冷飲的總容量: %,d cc\n", coldDrinkVolume);
+		
+		// 利用 Stream API 求解
+		int coldDrinkVolume2 = Arrays.stream(drinks)
+									 .filter(drink -> drink.temperature < 5) // Drink("冰茶", 2, 700), Drink("果汁", 4, 300)
+									 .mapToInt(drink -> drink.volume)        // 700, 300
+									 .sum(); // 1000
+		System.out.printf("冷飲的總容量: %,d cc\n", coldDrinkVolume2);
+									 
 		
 
 	}
