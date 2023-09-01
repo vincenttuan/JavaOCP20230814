306677e82b18fb9820629fcb11fde41091795e5c
teacher <teacher@BE-403->
2023-09-01 21:07:10 +0800

    java

---
+++ src/day06/DrinkDemo2.java
@@ -0,0 +1,23 @@
+package day06;
+
+public class DrinkDemo2 {
+
+	public static void main(String[] args) {
+		Drink drink1 = new Drink("咖啡", 75, 500);
+		Drink drink2 = new Drink("冰茶", 2, 700);
+		Drink drink3 = new Drink("豆漿", 35, 600);
+		Drink drink4 = new Drink("牛奶", 50, 200);
+		Drink drink5 = new Drink("果汁", 10, 300);
+		
+		Drink[] drinks = {drink1, drink2, drink3, drink4, drink5};
+		// 請透過 drinks 完成以下需求
+		// 1.加熱咖啡
+		// 2.冰果汁
+		// 3.喝牛奶分別喝 5cc 與 20cc
+		// 4.印出飲料資料
+		
+		
+
+	}
+
+}
