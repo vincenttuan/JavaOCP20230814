30cdcb63744fd870fbe5949ecb47b17808b3d007
teacher <teacher@BE-403->
2023-09-01 19:42:25 +0800

    java

--- src/day06/CarDemo5.java
+++ src/day06/CarDemo5.java
@@ -1,6 +1,6 @@
 package day06;
 
-public class CarDemo4 {
+public class CarDemo5 {
 
 	public static void main(String[] args) {
 		// 在建立 Car 物件的同時就將 color 與 speed 設定好
@@ -12,20 +12,8 @@ public class CarDemo4 {
 		Car[] cars = {car1, car2, car3};
 		// 請將黑車加速一次, 紅車減速二次
 		// 最後印出每一台車的顏色與車速
-		for(Car car : cars) {
-			switch (car.color) {
-				case "黑":
-					car.accelerate(); // 加速一次
-					break;
-				case "紅":
-					// 減速二次
-					car.brake(); 
-					car.brake();
-					break;
-			}
-			// 印出資訊
-			System.out.printf("%s, %d\n", car.color, car.speed);
-		}
+		// 請用 Java 8 Stream 實現
+		
 			
 	}
 
