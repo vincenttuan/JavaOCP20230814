0362150482fc2e9a01575ffb1c6dc373f5ff29a4
teacher <teacher@BE-403->
2023-09-01 21:26:07 +0800

    java

--- src/day06/DrinkDemo3.java
+++ src/day06/DrinkDemo3.java
@@ -8,11 +8,12 @@ public class DrinkDemo3 {
 		Drink drink1 = new Drink("咖啡", 75, 500);
 		Drink drink2 = new Drink("冰茶", 2, 700);
 		Drink drink3 = new Drink("豆漿", 35, 600);
-		Drink drink4 = new Drink("牛奶", 50, 200);
-		Drink drink5 = new Drink("果汁", 10, 300);
+		Drink drink4 = new Drink("牛奶", 65, 200);
+		Drink drink5 = new Drink("果汁", 4, 300);
 		
 		Drink[] drinks = {drink1, drink2, drink3, drink4, drink5};
 		// 請計算出冷熱飲各有多少容量
+		// 飲料溫度 (小於5度表示冷飲, 大於60度表示熱飲)
 		
 
 	}
