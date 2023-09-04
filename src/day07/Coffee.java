826caa684659f2dd1550d75087507eb3e88dcc2a
teacher <teacher@BE-403->
2023-09-04 21:15:37 +0800

    java

---
+++ src/day07/Coffee.java
@@ -0,0 +1,7 @@
+package day07;
+
+public class Coffee {
+	private String name;
+	private int price;
+	private String roast; // 烘培法
+}
---
+++ src/day07/Juice.java
@@ -0,0 +1,8 @@
+package day07;
+
+public class Juice {
+	private String name;
+	private int price;
+	private double content; // 果汁含量 0.2(20%)
+	
+}
