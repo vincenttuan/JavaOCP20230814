c5a7650c6594bd17779c36cb9896a30e7c13fc57
teacher <teacher@BE-403->
2023-09-08 19:13:10 +0800

    java

--- src/day08/Change.java
+++ src/day08/Change.java
@@ -16,10 +16,18 @@ public class Change {
 		int change = amount - price;
 		System.out.printf("找零金額: %d 元\n", change);
 		// 計算每個銅板(50, 10, 5, 1)各要找幾個 ?
-		int coin50 = 0;
-		int coin10 = 0;
-		int coin5 = 0;
-		int coin1 = 0;
+		// 假設 change = 73
+		int coin50 = change / 50; // 73/50 = 1 個 50
+		change = change % 50;     // 73 % 50 = 23 ... change
+		
+		int coin10 = change / 10; // 23 / 10 = 2 個 10
+		change = change % 10;     // 23 % 10 = 3 ... change
+		
+		int coin5 = change / 5;   // 3 / 5 = 0 個 5
+		change = change % 5;      // 3 % 5 = 3 ... change 
+		
+		int coin1 = change;
+		
 		System.out.printf("50元 %d 個\n", coin50);
 		System.out.printf("10元 %d 個\n", coin10);
 		System.out.printf("5元 %d 個\n", coin5);
