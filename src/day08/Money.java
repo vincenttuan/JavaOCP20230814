d3120c82f10709948b5ba8cc80b02ceb58f86abf
teacher <teacher@BE-403->
2023-09-08 19:34:44 +0800

    java

---
+++ src/day08/Money.java
@@ -0,0 +1,13 @@
+package day08;
+
+public class Money {
+	protected int value;
+	
+	public Money(int value) {
+		this.value = value;
+	}
+	
+	public void display() {
+		System.out.printf("價值 %d\n", value);
+	}
+}
