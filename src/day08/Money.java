0b3c8b69febb996173708840f485cca9ef286a65
teacher <teacher@BE-403->
2023-09-08 20:50:14 +0800

    java

--- src/day08/Money.java
+++ src/day08/Money.java
@@ -2,6 +2,7 @@ package day08;
 
 public class Money {
 	protected int value;
+	protected int count;
 	
 	public Money(int value) {
 		this.value = value;
@@ -9,6 +10,12 @@ public class Money {
 	
 	public int getValue() {
 		return value;
+	}
+	
+	// 可以換幾個(張)
+	public int computeCount(int change) {
+		count = change / value;
+		return count;
 	}
 	
 	public void display() {
