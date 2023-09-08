472d9fbd711a19d966b7f1f2b0f77052f6aaa945
teacher <teacher@BE-403->
2023-09-08 19:37:36 +0800

    java

---
+++ src/day08/Note.java
@@ -0,0 +1,15 @@
+package day08;
+
+public class Note extends Money {
+	private String label;
+	
+	public Note(int value, String label) {
+		super(value); // 呼叫 Money 的建構子
+		this.label = label;
+	}
+	
+	public void display() {
+		System.out.printf("%s 價值 %d\n", label, value);
+	}
+	
+}
