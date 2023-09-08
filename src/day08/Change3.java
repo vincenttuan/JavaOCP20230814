fc19aa6733b19fcd1c81944e870bc69b1b24f6bb
teacher <teacher@BE-403->
2023-09-08 20:56:12 +0800

    java

---
+++ src/day08/Change3.java
@@ -0,0 +1,28 @@
+package day08;
+
+public class Change3 {
+
+	public static void main(String[] args) {
+		Money[] moneyArray = {
+				new Note(500, "500元"),
+				new Note(100, "100元"),
+				new Coin(50, "50元"),
+				new Coin(10, "10元"),
+				new Coin(5, "5元"),
+				new Coin(1, "1元"),
+		}; 
+		
+		int price = 23;
+		int amount = 1000;
+		int change = amount - price;
+		System.out.printf("找零金額: %d\n", change);
+		
+		for(Money money : moneyArray) {
+			int count = money.computeCount(change); // 得到個(張)數
+			change = change - (money.getValue() * count); // 減去已經找零的
+			money.display();
+		}
+
+	}
+
+}
