16089e0a5c4ba04dae814aaf5251c8d4f093e1ca
teacher <teacher@BE-403->
2023-09-18 21:07:18 +0800

    java

--- src/pattern/decorator/Subway.java
+++ src/pattern/decorator/Subway.java
@@ -3,7 +3,21 @@ package pattern.decorator;
 public class Subway {
 	public static void main(String[] args) {
 		Food food = new Bread();
+		food = new Ham(food);
+		food = new Egg(food);
+		food = new Egg(food);
+		food = new Egg(food);
+		food = new Olive(food);
+		food = new Tomato(food);
+		food = new Tomato(food);
+		food = new Tomato(food);
+		food = new Tomato(food);
+		food = new Tuna(food);
+		food = new Tuna(food);
+		food = new Lettuce(food);
+		food = new Lettuce(food);
 		
-		
+		System.out.println(food.getName());
+		System.out.println(food.getPrice());
 	}
 }
