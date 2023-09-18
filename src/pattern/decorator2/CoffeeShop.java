e944c29898a752b5edb46f16be3d58875ff36cd1
teacher <teacher@BE-403->
2023-09-18 21:36:00 +0800

    java

--- src/pattern/decorator2/CoffeeShop.java
+++ src/pattern/decorator2/CoffeeShop.java
@@ -4,13 +4,22 @@ public class CoffeeShop {
 
 	public static void main(String[] args) {
 		Coffee coffee = new BasicCoffee();
-		coffee = new MilkDecorator(coffee);
-		coffee = new SugarDecorator(coffee);
-		coffee = new FoamDecorator(coffee);
+		print(coffee);
 		
+		coffee = new MilkDecorator(coffee);
+		print(coffee);
+		
+		coffee = new SugarDecorator(coffee);
+		print(coffee);
+		
+		coffee = new FoamDecorator(coffee);
+		print(coffee);
+		
+	}
+	
+	private static void print(Coffee coffee) {
 		System.out.println(coffee.getDesp());
 		System.out.println(coffee.getCost());
-
 	}
 
 }
