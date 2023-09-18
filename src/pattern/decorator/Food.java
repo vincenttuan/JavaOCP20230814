3119371d25f355eac56669532a8efd93b2c3d960
teacher <teacher@BE-403->
2023-09-18 20:54:59 +0800

    java

---
+++ src/pattern/decorator/Food.java
@@ -0,0 +1,9 @@
+package pattern.decorator;
+
+public abstract class Food {
+	protected String name;
+	protected int price;
+	
+	public abstract String getName();
+	public abstract int getprice();
+}
