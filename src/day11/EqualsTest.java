83d2f80612ebc979f531c11267e54f98f8ca9611
teacher <teacher@BE-403->
2023-09-22 21:23:28 +0800

    java

---
+++ src/day11/EqualsTest.java
@@ -0,0 +1,15 @@
+package day11;
+
+public class EqualsTest {
+
+	public static void main(String[] args) {
+		Pen p1 = new Pen("Red", 10);
+		Pen p2 = new Pen("Blue", 10);
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p1 == p2);
+		System.out.println(p1.equals(p2));
+
+	}
+
+}
--- src/day11/Pen.java
+++ src/day11/Pen.java
@@ -1,5 +1,7 @@
 package day11;
 
+import java.util.Objects;
+
 public class Pen {
 	private String color;
 	private int price;
@@ -8,6 +10,20 @@ public class Pen {
 		this.color = color;
 		this.price = price;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == this) {
+			return true;
+		}
+		if(!(obj instanceof Pen)) {
+			return false;
+		}
+		Pen pen = (Pen)obj;
+		return Objects.equals(color, pen.color) && price == pen.price;
+	}
+
+
 
 	@Override
 	public String toString() {
