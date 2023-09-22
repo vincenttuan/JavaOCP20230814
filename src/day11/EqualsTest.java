101d7554a3f26507bc82de91050f5e5edd99477d
teacher <teacher@BE-403->
2023-09-22 21:24:51 +0800

    java

--- src/day11/EqualsTest.java
+++ src/day11/EqualsTest.java
@@ -4,12 +4,13 @@ public class EqualsTest {
 
 	public static void main(String[] args) {
 		Pen p1 = new Pen("Red", 10);
-		Pen p2 = new Pen("Blue", 10);
+		Pen p2 = new Pen("Red", 10);
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p1 == p2);
 		System.out.println(p1.equals(p2));
-
+		System.out.println(p1.hashCode());
+		System.out.println(p2.hashCode());
 	}
 
 }
