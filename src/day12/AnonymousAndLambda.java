e23d4f9456186897f186ecf6d90a904e26c74a83
teacher <teacher@BE-403->
2023-10-02 21:30:59 +0800

    java

---
+++ src/day12/AnonymousAndLambda.java
@@ -0,0 +1,68 @@
+package day12;
+
+interface Greet {
+	void hello();
+	//void welcome();
+}
+
+interface Calc {
+	int max(int x, int y);
+}
+
+public class AnonymousAndLambda {
+
+	public static void main(String[] args) {
+		// 匿名內部類別
+		Greet greet1 = new Greet() {
+			@Override
+			public void hello() {
+				System.out.println("Hello World!");
+			}
+		};
+		greet1.hello();
+		
+		// Lambda
+		Greet greet2 = () -> System.out.println("Hello World!");
+		greet2.hello();
+		
+		// Lambda 標準寫法
+		Calc calc1 = (int x, int y) -> {
+			if(x > y) {
+				return x;
+			} else {
+				return y;
+			}
+		};
+		
+		// Lambda 簡化寫法 1
+		Calc calc2 = (var x, var y) -> {
+			if(x > y) {
+				return x;
+			} else {
+				return y;
+			}
+		};
+		
+		// Lambda 簡化寫法 2
+		Calc calc3 = (x, y) -> {
+			if(x > y) {
+				return x;
+			} else {
+				return y;
+			}
+		};
+		
+		// Lambda 簡化寫法 3
+		// 若實作只有一行可以省略 return
+		Calc calc4 = (x, y) -> x > y ? x : y;
+		
+		
+	}
+
+}
+
+
+
+
+
+
