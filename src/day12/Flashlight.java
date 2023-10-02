7a5d4480d52a81cc62ee75c28c59657e58ea50ce
teacher <teacher@BE-403->
2023-10-02 20:06:52 +0800

    java

--- src/day12/Flashlight.java
+++ src/day12/Flashlight.java
@@ -5,14 +5,22 @@ public class Flashlight {
 	// 靜態內部類別
 	// 他可以獨立於手電筒而存在, 但是仍然屬於手電筒
 	static class Battery {
-		static void charge() { // static 可以加可不加
+		int capacity = 100;
+		void charge1() { // 要建立 Battery 實例才可以調用 
+			System.out.println(capacity + "電池正在充電中");
+		}
+		
+		static void charge2() { // 不須要建立 Battery 實例就可以調用
 			System.out.println("電池正在充電中");
 		}
 	}
 	
 	public static void main(String[] args) {
 		Flashlight.Battery battery = new Flashlight.Battery();
-		battery.charge();
+		battery.charge1();
+		
+		Flashlight.Battery.charge2();
+		
 	}
 	
 }
