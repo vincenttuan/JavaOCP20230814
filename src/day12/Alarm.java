9f14a735376a7d5d529e1288bff1ed43dde2e7e1
teacher <teacher@BE-403->
2023-10-02 20:40:27 +0800

    java

---
+++ src/day12/Alarm.java
@@ -0,0 +1,28 @@
+package day12;
+
+public class Alarm {
+	
+	// 時間介面
+	interface Time {
+		void ring();
+	}
+	
+	void setAlarm(int hours) {
+		Time time = new Time() {
+			
+			@Override
+			public void ring() {
+				System.out.printf("過了%d小時!鬧鐘響了!\n", hours);
+			}
+			
+		};
+		
+		time.ring();
+	}
+	
+	// 測試
+	public static void main(String[] args) {
+		Alarm alarm = new Alarm();
+		alarm.setAlarm(3);
+	}
+}
