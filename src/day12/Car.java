0f0473553bb3448723b6f3d9941bd8ec268bcd31
teacher <teacher@BE-403->
2023-10-02 21:09:42 +0800

    java

---
+++ src/day12/Car.java
@@ -0,0 +1,34 @@
+package day12;
+
+public class Car {
+	// 靜態內部類別: 表示汽車的引擎
+	static class Engine {
+		void start() {
+			System.out.println("引擎啟動 ");
+		}
+	}
+	
+	// 一般內部類別: 表示汽車音響系統
+	class AudioSystem {
+		void playMusic() {
+			System.out.println("播放音樂...");
+		}
+	}
+	
+	private Engine engine = new Engine();
+	
+	public void startCar() {
+		engine.start();
+		// 方法內部類別: 表示汽車啟動時的安全檢查
+		class SafetyCheck {
+			void doCheck() {
+				System.out.println("執行安全檢查...");
+			}
+		}
+		SafetyCheck check = new SafetyCheck();
+		check.doCheck();
+	}
+	
+	
+	// 匿名內部類別: 表示汽車內的空調 
+}
