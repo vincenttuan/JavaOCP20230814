a24675aeac81c62a471d3bb3beb3d67d13a1af39
teacher <teacher@BE-403->
2023-10-06 21:22:27 +0800

    java

---
+++ src/day13/StreamDemo2.java
@@ -0,0 +1,11 @@
+package day13;
+
+public class StreamDemo2 {
+	public static void main(String[] args) {
+		// 1.得到成績列表 95.5, 43.5, 30,8, 75.9, 100.0
+		// 2.過濾出及格的分數
+		// 3.將分數(及格)加權10%
+		// 4.印出分數
+			
+	}
+}
