53a4163b60fc381f7bbacc92591f464977d78903
teacher <teacher@BE-403->
2023-10-13 21:10:16 +0800

    java

--- src/day14/SetDemo4.java
+++ src/day14/SetDemo4.java
@@ -6,7 +6,7 @@ import java.util.TreeSet;
 
 public class SetDemo4 {
 	public static void main(String[] args) {
-		// 利用 Stream API
+		// 1. 利用 Stream API 取 1..9 五個不重複的數字
 		Set<Integer> numbers = new TreeSet<>();
 		
 		new SecureRandom().ints(1, 10) // 產生一個隨機數字流, 範圍是 1..9
@@ -16,5 +16,17 @@ public class SetDemo4 {
 						  .forEach(numbers::add); // 添加到集合
 		
 		System.out.println(numbers);
+		
+		// 2. 利用 Stream API 取 1..9 三個不重複的偶數 (利用: filter)
+		Set<Integer> numbers2 = new TreeSet<>();
+		
+		new SecureRandom().ints(1, 10) // 產生一個隨機數字流, 範圍是 1..9
+						  .filter(num -> num % 2 == 0) // 過濾出偶數
+						  .distinct()
+						  .limit(3)
+						  .forEach(numbers2::add); // 添加到集合
+
+		System.out.println(numbers2);
+		
 	}
 }
