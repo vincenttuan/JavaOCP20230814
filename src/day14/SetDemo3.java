d9445ab91604ed620e6321ad92c26496e01d88da
teacher <teacher@BE-403->
2023-10-13 20:42:01 +0800

    java

---
+++ src/day14/SetDemo3.java
@@ -0,0 +1,20 @@
+package day14;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo3 {
+
+	public static void main(String[] args) {
+		// 元素預設會進行自然排序(小 -> 大)
+		Set<Integer> numbers = new TreeSet<>();
+		// 初始集合內容
+		System.out.println(numbers.size());
+		System.out.println(numbers);
+		
+		// 1~9 取 5 個不重複的數字並放入到 numbers 中
+		
+
+	}
+
+}
