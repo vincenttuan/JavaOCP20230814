0705d6d0a7a4386137d54e9069d0d91da5326953
teacher <teacher@BE-403->
2023-10-16 20:50:41 +0800

    java

---
+++ src/day15/ListDemo3.java
@@ -0,0 +1,24 @@
+package day15;
+
+/*
+可變集合/不可變集合/不可修改集合
+
+           +-----+-----+--------+
+           | add | set | remove |
++----------+-----+-----+--------+
+| 可以變的集合 |  V  |  V  |   V    | new ArrayList(), new LinkedList()...
++----------+-----+-----+--------+
+| 不可變的集合 |  X  |  V  |   X    | Arrays.asList(...)
++----------+-----+-----+--------+
+| 不可修改集合 |  X  |  X  |   X    | Collections.unmodifiableList(...), List.of(...) ...
++----------+-----+-----+--------+
+ 
+*/
+public class ListDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
