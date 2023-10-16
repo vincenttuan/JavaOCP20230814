8913ca4a77eae3f2488318b6b5a60780dd505705
teacher <teacher@BE-403->
2023-10-16 19:33:47 +0800

    java

--- src/day14/BookSetDemo2.java
+++ src/day14/BookSetDemo2.java
@@ -12,7 +12,7 @@ public class BookSetDemo2 {
 		Book b4 = new Book("Web", 500);
 		Book b5 = new Book("C/C++", 100);
 		
-		Set<Book> books = new TreeSet<>();
+		TreeSet<Book> books = new TreeSet<>();
 		books.add(b1);
 		books.add(b2);
 		books.add(b3);
@@ -21,7 +21,8 @@ public class BookSetDemo2 {
 		
 		System.out.println(books.size());
 		books.forEach(System.out::println);
-		
+		System.out.println();
+		books.descendingSet().forEach(System.out::println);
 		
 	}
 	
