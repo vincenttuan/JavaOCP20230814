35628aa4c124e89bac1a4b704b42c42c9f7bb7a2
teacher <teacher@BE-403->
2023-10-16 19:38:11 +0800

    java

--- src/day14/BookSetDemo2.java
+++ src/day14/BookSetDemo2.java
@@ -20,7 +20,18 @@ public class BookSetDemo2 {
 		books.add(b5);
 		
 		System.out.println(books.size());
+		
+		// price 由小到大
 		books.forEach(System.out::println);
+		System.out.println("最貴的書: " + books.last());
+		System.out.println("最便宜的書: " + books.first());
+		// 金額 price >= b2 的書
+		//books.tailSet(b2).forEach(System.out::println);
+		// 金額 price >= 350 的書
+		books.tailSet(new Book("", 350))
+			 .forEach(System.out::println);
+		
+		// price 由大到小
 		System.out.println();
 		books.descendingSet().forEach(System.out::println);
 		
