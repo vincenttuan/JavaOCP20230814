ef80d3ce7680df11382d379934b7394b767a97b8
teacher <teacher@BE-403->
2023-10-16 19:58:34 +0800

    java

---
+++ src/day15/ListDemo1.java
@@ -0,0 +1,14 @@
+package day15;
+
+/*
+ * ArrayList: 連續的動態數組,希望能創造O(1)的隨機訪問, 但是需要調整大小結構, 刪 (慢)
+ * LinkedList: 元素與元素之間都有參考點, 透過參考形成連續的數組, 隨機訪問比 ArrayList 慢, 但是刪 (快)
+ * */
+public class ListDemo1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
