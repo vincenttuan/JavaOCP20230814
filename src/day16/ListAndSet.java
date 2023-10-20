3e5854becadf842c3c14ae6137ba63a1b745dac6
teacher <teacher@BE-403->
2023-10-20 20:11:04 +0800

    java

--- src/day16/ListAndSet.java
+++ src/day16/ListAndSet.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 /*
  * [ <-- List
@@ -27,11 +28,11 @@ public class ListAndSet {
 		List<String> lines = Files.readAllLines(Paths.get("src/day16/sales_data.txt"));
 		
 		// Java 7 (含) 以前的寫法
-		List<Set<String>> salesList = new ArrayList<>();
+		Set<List<String>> salesList = new LinkedHashSet<>();
 		for(int k=1;k<lines.size();k++) { // 從 1 開始, 因為 0 是標頭資料
 			// 將 line 利用 , 切開
 			String[] array = lines.get(k).split(",");
-			Set<String> sales = new LinkedHashSet<>();
+			List<String> sales = new ArrayList<>();
 			for(int i=0;i<array.length;i++) {
 				sales.add(array[i]); // 將每一個元素放到 sales 集合
 			}
@@ -40,12 +41,22 @@ public class ListAndSet {
 		System.out.println(salesList);
 		
 		// 利用 Java Stream Api 改寫成更加簡潔且易於閱讀的程式碼
-		List<Set<String>> salesList2 = lines.stream() // 轉換 stream
+		Set<List<String>> salesList2 = lines.stream() // 轉換 stream
 				.skip(1) // 跳過標頭資料
 				.map(line -> stream(line.split(",")) // 將每一行以 "," 切分, 並轉 stream 以利後續處理
-							 .collect(toCollection(LinkedHashSet::new))) // 收集結果並放在 LinkedHashSet 集合中 
-				.collect(toList());
+							 .collect(toCollection(ArrayList::new))) // 收集結果並放在 LinkedHashSet 集合中 
+				.collect(toSet());
 		System.out.println(salesList2);
+		
+		// 分析總銷售金額 
+		int totalSales = salesList2.stream()
+				.mapToInt(sales -> {
+					String priceStr = sales.stream().skip(3).findFirst().get();
+					String qtyStr = sales.stream().skip(4).findFirst().get();
+					return Integer.parseInt(priceStr) * Integer.parseInt(qtyStr);
+				})
+				.sum();
+		System.out.println("總銷售金額: " + totalSales);
 		
 	}
 	
