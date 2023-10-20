7f6eb4c283af62d77fc70766c3001160b557d435
teacher <teacher@BE-403->
2023-10-20 21:42:57 +0800

    java

--- src/day16/MapDemo3.java
+++ src/day16/MapDemo3.java
@@ -1,5 +1,6 @@
 package day16;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -14,10 +15,18 @@ public class MapDemo3 {
 		// 請計算每種水果的個數
 		// apple=3, banana=2, orange=1, papaya=1
 		Map<String, Long> result = fruits.stream().collect(
-					Collectors.groupingBy(name->name+".TW", Collectors.counting())
-					//Collectors.groupingBy(Function.identity(), Collectors.counting())
+					//Collectors.groupingBy(name->name+".TW", Collectors.counting())
+					Collectors.groupingBy(Function.identity(), Collectors.counting())
 				);
 		System.out.println(result);
+		
+		// 排序 by value (comparingByValue)
+		Map<String, Long> sortedResult = new LinkedHashMap<>();
+		result.entrySet().stream()
+						 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+						 .forEach(entry -> sortedResult.put(entry.getKey(), entry.getValue()));
+		
+		System.out.println(sortedResult);
 	}
 
 }
