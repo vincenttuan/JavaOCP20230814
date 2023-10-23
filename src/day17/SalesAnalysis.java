a9d3019d05f74f78e5457421ebb036c67299b076
teacher <teacher@BE-403->
2023-10-23 18:47:54 +0800

    java

---
+++ src/day17/SalesAnalysis.java
@@ -0,0 +1,13 @@
+package day17;
+
+// 分析 sales_data.json
+// 1. 建立 SaleRecord 物件
+// 2. 將 sales_data.json 的資料匯入到集合中 List<SalesRecord>
+public class SalesAnalysis {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
