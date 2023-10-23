226e4fd833e3fe26da242510cd198f8be1e4254e
teacher <teacher@BE-403->
2023-10-23 18:49:52 +0800

    java

---
+++ src/day17/SaleRecord.java
@@ -0,0 +1,61 @@
+package day17;
+
+public class SaleRecord {
+	private String no;
+	private String date;
+	private String product;
+	private Integer price;
+	private Integer qty;
+	private String city;
+	private String branch;
+	
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Integer getQty() {
+		return qty;
+	}
+	public void setQty(Integer qty) {
+		this.qty = qty;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	@Override
+	public String toString() {
+		return "SaleRecord [no=" + no + ", date=" + date + ", product=" + product + ", price=" + price + ", qty=" + qty
+				+ ", city=" + city + ", branch=" + branch + "]";
+	}
+	
+	
+}
