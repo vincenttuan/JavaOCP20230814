1b155e82f5e8e0a677ed86e2be293de53c0ea822
teacher <teacher@BE-403->
2023-10-23 18:56:54 +0800

    java

--- src/day17/SalesAnalysis.java
+++ src/day17/SalesAnalysis.java
@@ -1,13 +1,24 @@
 package day17;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson;
+
 // 分析 sales_data.json
 // 1. 建立 SaleRecord 物件
 // 2. 將 sales_data.json 的資料匯入到集合中 List<SalesRecord>
 public class SalesAnalysis {
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		Gson gson = new Gson();
+		FileReader reader = new FileReader("src/day17/sales_data.json");
+		// TypeToken 可以幫助 Gson 知道要轉換的類型
+		List<SaleRecord> saleRecords = gson.fromJson(reader, new TypeToken<List<SaleRecord>>() {}.getType());
+		System.out.println(saleRecords.size());
+		System.out.println(saleRecords);
 	}
 
 }
