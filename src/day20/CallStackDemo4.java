9f8c71c1c61fffd8e4adba7d796c920b98aea495
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-03 20:26:12 +0800

    java

---
+++ src/day20/CallStackDemo4.java
@@ -0,0 +1,36 @@
+package day20;
+
+// 自訂錯誤類
+class InvalidYArgException extends Exception {
+	InvalidYArgException(String errorMessage) {
+		super(errorMessage);
+	}
+}
+
+public class CallStackDemo4 {
+
+	public static void main(String[] args) {
+		System.out.println("begin");
+		int x = 10;
+		int y = 0;
+		
+		try {
+			div(x, y);
+		} catch (InvalidYArgException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("end");
+	}
+	
+	public static void div(int x, int y) throws InvalidYArgException {
+		// 檢查參數
+		if (y == 0) {
+			InvalidYArgException re = new InvalidYArgException("分母不可 = 0");
+			throw re;
+		}
+		// 執行
+		System.out.println(x / y);
+	}
+
+}
