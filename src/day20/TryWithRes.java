d22201279c6e693e6ae15e1bb1ed116e9a7ec3b7
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-03 19:51:56 +0800

    java

--- src/day20/TryWithRes.java
+++ src/day20/TryWithRes.java
@@ -1,17 +1,13 @@
 package day20;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class TryWithRes {
 
 	public static void main(String[] args) {
-		
-		try {
-			System.out.println(10/0);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
 		
 		try(Scanner sc = new Scanner(System.in)) {
 			System.out.println(10/0);
@@ -26,6 +22,29 @@ public class TryWithRes {
 		} finally {
 			if(sc != null) {
 				sc.close();
+			}
+		}
+		
+		
+		try(Connection conn = DriverManager.getConnection("dburl")) {
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+		
+		// 同等於
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("dburl");
+		} catch (SQLException e) {
+			// TODO: handle exception
+		} finally {
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO: handle exception
+				}
 			}
 		}
 		
