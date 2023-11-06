292bc61b2d885c1f2be8b632a7e3237c81efe604
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-06 19:31:52 +0800

    java

--- src/test/TestCalc.java
+++ src/test/TestCalc.java
@@ -23,8 +23,23 @@ public class TestCalc {
 		
 		// Assert(期望得到的值)
 		int value = 5;
-		assertEquals("add", value , result);
+		assertEquals("add error1", value , result);
+	}
+	
+	@Test
+	// 自行使用 assert
+	public void test2() {
+		// Arrange(安排)
+		Calc calc = new Calc();
+		int x = 2;
+		int y = 3;
 		
+		// Act(實際得到的值)
+		int result = calc.add(x, y);
+		
+		// Assert(期望得到的值)
+		int value = 5;
+		assert(value == result): "add error2";
 	}
 	
 	
