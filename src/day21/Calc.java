30be4bfd6fe20d4fa9360f4bcddfd573025ba0e8
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-06 19:26:27 +0800

    java

---
+++ src/day21/Calc.java
@@ -0,0 +1,8 @@
+package day21;
+
+public class Calc {
+	public int add(int x, int y) {
+		int sum = x + y;
+		return sum;
+	}
+}
--- src/test/TestCalc.java
+++ src/test/TestCalc.java
@@ -1,12 +1,30 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import day21.Calc;
 
 public class TestCalc {
 	
 	@Test
+	// 測試腳本
+	// 利用 3A 法則來測試物件資源/服務
+	// Arrange(安排), Act(實際得到的值), Assert(期望得到的值)
 	public void test1() {
-		System.out.println("Test");
+		// Arrange(安排)
+		Calc calc = new Calc();
+		int x = 2;
+		int y = 3;
+		
+		// Act(實際得到的值)
+		int result = calc.add(x, y);
+		
+		// Assert(期望得到的值)
+		int value = 5;
+		assertEquals("add", value , result);
+		
 	}
 	
 	
