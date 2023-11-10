b25fa370546648613f44cf9ae227389531e945f5
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-10 19:24:37 +0800

    java

---
+++ src/day22/ThreadDemo.java
@@ -0,0 +1,16 @@
+package day22;
+
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		Runnable r1 = new Program();
+		Runnable r2 = new Art();
+		Runnable r3 = new Performer();
+		
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r2);
+		Thread t3 = new Thread(r3);
+
+	}
+
+}
