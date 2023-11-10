5e648c889fe53d7320cb39f4fd62082e49eb2fde
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-10 21:15:09 +0800

    java

--- src/day22/Exchange2Demo.java
+++ src/day22/Exchange2Demo.java
@@ -7,9 +7,12 @@ public class Exchange2Demo {
 	public static void main(String[] args) {
 		int x = 100;
 		
-		Consumer<Integer> callback = (ex) -> System.out.println("x = " + (x * ex));
+		Consumer<Integer> callback1 = (ex) -> System.out.println("x = " + (x * ex));
+		Consumer<Integer> callback2 = (ex) -> System.out.println("x = " + (x / ex));
+		Consumer<Integer> callback3 = (ex) -> System.out.println("x = " + (x + ex));
+		Consumer<Integer> callback4 = (ex) -> System.out.println("x = " + (x - ex));
 		
-		Thread t1 = new Exchange2(callback);
+		Thread t1 = new Exchange2(callback3);
 		t1.start();
 		
 	}
