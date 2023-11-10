54f0d7b13fb428bab213896137e41b56cd8bb29b
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-10 20:39:48 +0800

    java

--- src/day22/Exchange.java
+++ src/day22/Exchange.java
@@ -4,11 +4,18 @@ import java.util.Random;
 
 // 取得匯率
 public class Exchange extends Thread {
+	private Callback callback;
+	
+	public Exchange(Callback callback) {
+		this.callback = callback;
+	}
 	
 	public void run() {
 		// 目前匯率
 		int ex = (new Random().nextInt(10)) + 10;
 		System.out.println("目前匯率: " + ex);
+		// 將匯率設定給 callback
+		callback.result(ex);
 	}
 	
 }
--- src/day22/ExchangeDemo.java
+++ src/day22/ExchangeDemo.java
@@ -1,9 +1,20 @@
 package day22;
 
+interface Callback {
+	void result(int ex);
+}
+
 public class ExchangeDemo {
 	public static void main(String[] args) {
-		Thread t1 = new Exchange();
-		t1.start();
+		int x = 100;
 		
+		Callback callback = (ex) -> {
+			// x = x * 匯率(ex)
+			System.out.println("x = " + (x * ex));
+		};
+		
+		Thread t1 = new Exchange(callback);
+		t1.start();
+				
 	}
 }
