ea3bb625acd96013816f05dc324ce39ecfde9e12
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-10 21:45:57 +0800

    java

--- src/day22/TransactionMain.java
+++ src/day22/TransactionMain.java
@@ -2,18 +2,23 @@ package day22;
 
 public class TransactionMain {
 	public static void main(String[] args) {
-		TransactionCallback callback = (result, amount, price) -> {
+		TransactionCallback callback1 = (result, amount, price) -> {
 			System.out.println(result);
 			System.out.println("應付款的金額 $" + (amount * price));
+		};
+		
+		ShoppingCallback callback2 = message -> {
+			System.out.println("老婆說: " + message);
+			System.out.println("老公去付錢");
 		};
 		
 		System.out.println("開啟交易系統");
 		
 		// 執行交易 
-		Thread t1 = new TransactionTask(callback);
-		t1.start();
+		new TransactionTask(callback1).start();
+		new ShoppingTask(callback2).start();
 		
-		System.out.println("等待交易結果");
+		System.out.println("等待交易結果與老婆");
 		
 	}
 }
