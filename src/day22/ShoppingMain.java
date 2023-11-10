930f1b8daf094bb3945570fa4a8b538246183b5e
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-10 21:26:40 +0800

    java

---
+++ src/day22/ShoppingMain.java
@@ -0,0 +1,19 @@
+package day22;
+
+public class ShoppingMain {
+
+	public static void main(String[] args) {
+		// 帶老婆去購物
+		ShoppingCallback callback = message -> {
+			System.out.println("老婆說: " + message);
+			System.out.println("老公去付錢");
+		};
+		
+		System.out.println("老公帶老婆去購物");
+		Thread t1 = new ShoppingTask(callback);
+		t1.start();
+		System.out.println("老公在椅子上休息等老婆");
+
+	}
+
+}
