45280ade345db54812fffb756c53c854cb5ace3c
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-13 20:34:05 +0800

    java

---
+++ src/day23/BankAccount.java
@@ -0,0 +1,26 @@
+package day23;
+
+public class BankAccount {
+	private int balance;
+	
+	public BankAccount(int balance) {
+		this.balance = balance;
+	}
+	
+	// 提款
+	public void withdraw(int amount) {
+		String tName = Thread.currentThread().getName();
+		
+		// 目前帳戶餘額
+		int currentBalance = this.balance;
+		
+		if(currentBalance >= amount) {
+			System.out.printf("%s 提款 $%d 成功%n", tName, amount);
+			// 餘額減去提款金額
+			this.balance -= amount;
+		} else {
+			System.out.printf("%s 提款 $%d 失敗%n", tName, amount);
+		}
+		
+	}
+}
