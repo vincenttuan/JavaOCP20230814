4186252dfd1b5a91dba28995b86080a20128e97f
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-13 18:49:30 +0800

    java

---
+++ src/day23/Lotto.java
@@ -0,0 +1,15 @@
+package day23;
+
+import java.util.concurrent.Callable;
+
+// 可以得到 lotto 號碼的 callable(有回傳值)
+public class Lotto implements Callable<Integer[]> {
+
+	@Override
+	public Integer[] call() throws Exception {
+		// 取得 1~39 不重複的數字 5 個, 取得後放到 Integer[] 陣列中, 並回傳
+		
+		return null;
+	}
+
+}
