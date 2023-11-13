477395ea192b6f1ec03250921d60b383481ef3b0
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-13 21:29:49 +0800

    java

--- src/day23/BankAccount.java
+++ src/day23/BankAccount.java
@@ -12,22 +12,27 @@ public class BankAccount {
 	}
 
 	// 提款
+	//public synchronized void withdraw(int amount) {
 	public synchronized void withdraw(int amount) {
 		String tName = Thread.currentThread().getName();
-
-		// 目前帳戶餘額
-		int currentBalance = this.balance;
-
-		if (currentBalance >= amount) {
-			System.out.printf("%s 提款 $%d (成功) ", tName, amount);
-			// 餘額減去提款金額
-			this.balance -= amount;
-			System.out.printf("餘額 $%d%n", this.balance);
-		} else {
-			System.out.printf("%s 提款 $%d (失敗) 餘額 $%d%n", tName, amount, this.balance);
+		
+		lock.lock();
+		try {
+			// 目前帳戶餘額
+			int currentBalance = this.balance;
+			if (currentBalance >= amount) {
+				System.out.printf("%s 提款 $%d (成功) ", tName, amount);
+				// 餘額減去提款金額
+				this.balance -= amount;
+				System.out.printf("餘額 $%d%n", this.balance);
+			} else {
+				System.out.printf("%s 提款 $%d (失敗) 餘額 $%d%n", tName, amount, this.balance);
+			}
+		} finally {
+			lock.unlock();
 		}
-
 	}
+	
 	
 	// 存款
 	public void deposit(int amount) {
