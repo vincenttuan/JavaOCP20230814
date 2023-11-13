8ff4932c7ebd73b51867a23e879539c0ba620dd1
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-13 19:30:21 +0800

    java

--- src/day23/FileReaderAndWriter.java
+++ src/day23/FileReaderAndWriter.java
@@ -1,13 +1,15 @@
 package day23;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // 多執行緒檔案讀取與寫入
 // Runnable 負責寫入檔案
 // Callable 負責讀取檔案
 public class FileReaderAndWriter {
 	// 實現 Runnable 負責寫入檔案
-	class FileWriterTask implements Runnable {
+	static class FileWriterTask implements Runnable {
 		@Override
 		public void run() {
 			System.out.println("資料寫入到檔案");
@@ -15,7 +17,7 @@ public class FileReaderAndWriter {
 	}
 	
 	// 實現 Callable 負責讀取檔案
-	class FileReaderTask implements Callable<String> {
+	static class FileReaderTask implements Callable<String> {
 
 		@Override
 		public String call() throws Exception {
@@ -25,9 +27,18 @@ public class FileReaderAndWriter {
 		
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		ExecutorService service= Executors.newFixedThreadPool(2);
+		// 寫入文件工作
+		Runnable writeTask = new FileWriterTask();
+		// 讀取文件工作
+		Callable<String> readerTask = new FileReaderTask();
+		
+		// 執行任務工作
+		service.execute(writeTask);
+		service.submit(readerTask);
+		
+		service.shutdown();
 	}
 
 }
