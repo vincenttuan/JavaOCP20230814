78ca2e71a04a50c15a9dabda48633dc894f66bda
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-13 19:05:21 +0800

    java

--- src/day23/Lotto.java
+++ src/day23/Lotto.java
@@ -1,6 +1,8 @@
 package day23;
 
+import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.stream.IntStream;
 
 // 可以得到 lotto 號碼的 callable(有回傳值)
 public class Lotto implements Callable<Integer[]> {
@@ -9,7 +11,13 @@ public class Lotto implements Callable<Integer[]> {
 	public Integer[] call() throws Exception {
 		// 取得 1~39 不重複的數字 5 個, 取得後放到 Integer[] 陣列中, 並回傳
 		
-		return null;
+		Integer[] nums = IntStream.generate(() -> new Random().nextInt(39) + 1)
+								  .distinct()	
+								  .limit(5)
+								  .boxed()
+								  .toArray(Integer[]::new);
+		
+		return nums;
 	}
 
 }
