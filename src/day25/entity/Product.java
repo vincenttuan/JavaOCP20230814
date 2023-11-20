ee6878d0108ef87368c2e33ec5d1d79d74349dc4
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-20 19:05:35 +0800

    java

---
+++ src/day25/entity/Product.java
@@ -0,0 +1,18 @@
+package day25.entity;
+
+/**
+1. 商品
+Pack(盒), Box(箱), Bottle(瓶), Bag(包), Dozen(打)
++-----------+-------------+-------+--------+----------+
+| productId | productName | price |  unit  | isLaunch |
++-----------+-------------+-------+--------+----------+
+|    501    |   Coffee    |  300  |  Pack  |   true   |
+|    502    |  Green Tea  |  150  |  Box   |   false  |
+|    503    |   Honey     |  200  | Bottle |   false  |
+|    504    |   Sugar     |  100  |  Bag   |   true   |
+|    505    |   Milk      |  450  |  Dozen |   true   |
++-----------+-------------+-------+--------+----------+
+*/
+public class Product {
+	
+}
