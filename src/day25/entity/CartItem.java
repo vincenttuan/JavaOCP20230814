9409e219671763ffcc1e9ae9d7bdb71550ac732b
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-20 19:48:53 +0800

    java

--- src/day25/entity/CartItem.java
+++ src/day25/entity/CartItem.java
@@ -14,5 +14,27 @@ package day25.entity;
 +--------+----------+-----------+------------+
  * */
 public class CartItem {
+	private Integer itemId; // 購物車明細 Id
+	private Integer cartId; // 購物車 Id
+	private Cart cart; // 購物車物件
+	private Integer productId; // 商品 Id
+	private Product product; // 商品物件
+	private Integer quantity; // 數量
+	
+	public CartItem() {
+		
+	}
+	
+	public CartItem(Integer itemId, Integer cartId, Integer productId, Integer quantity) {
+		this.itemId = itemId;
+		this.cartId = cartId;
+		this.productId = productId;
+		this.quantity = quantity;
+		
+		// 根據 productId 找到 Product 物件
+		// 根據 cartId 找到 Cart 物件
+	}
+	
+	
 	
 }
