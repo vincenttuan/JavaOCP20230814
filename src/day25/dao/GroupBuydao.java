aeb7b749f25d8fe05a8d5fda2777db0e740235f2
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-20 20:04:10 +0800

    java

--- src/day25/dao/GroupBuydao.java
+++ src/day25/dao/GroupBuydao.java
@@ -1,7 +1,9 @@
 package day25.dao;
 
 import java.util.List;
+import java.util.Optional;
 
+import day25.entity.Product;
 import day25.entity.User;
 
 public interface GroupBuydao {
@@ -10,19 +12,29 @@ public interface GroupBuydao {
 	List<User> findAllUsers();
 	
 //	2. 新增使用者
-	
+	void addUser(User user);
 	
 //	3. 修改密碼
+	Boolean updateUserPassword(Integer userId, String newPassword);
 	
 //	4. 根據使用者名稱查找使用者(登入用-單筆)
+	Optional<User> findUserByUsername(String username);
 	
 //	5. 根據使用者ID查找使用者(單筆)
+	Optional<User> findUserById(Integer userId);
 //
 //	商品-Product
 //	1. 查詢所有商品(多筆)
+	List<Product> findAllProducts();
+	
 //	2. 根據產品ID來查找商品(單筆)
+	Optional<Product> findProductById(Integer productId);
+	
 //	3. 新增商品
+	void addProduct(Product product);
+	
 //	4. 變更商品上架狀態
+	Boolean updateProductLaunch(Integer productId, Boolean isLaunch);
 //
 //	購物車/購物車項目(Cart/CartItem)
 //	1. 新增購物車資料
