9ac5cf6bd7356dbfacfe3595a3c42095e70fb89f
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-24 19:23:59 +0800

    java

---
+++ src/day25/dao/GroupBuyDaoMySQL.java
@@ -0,0 +1,159 @@
+package day25.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import java.util.Optional;
+
+import day25.entity.Cart;
+import day25.entity.CartItem;
+import day25.entity.Product;
+import day25.entity.User;
+
+public class GroupBuyDaoMySQL implements GroupBuyDao {
+	private static GroupBuyDao _instance = new GroupBuyDaoMySQL();
+	private Connection conn;
+	
+	private GroupBuyDaoMySQL() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/groupbuy", "root", "12345678");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		conn.close();
+	}
+
+
+
+	@Override
+	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Boolean updateUserPassword(Integer userId, String newPassword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<User> findUserByUsername(String username) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<User> findUserById(Integer userId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<Product> findAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Product> findProductById(Integer productId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public void addProduct(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Boolean updateProductLaunch(Integer productId, Boolean isLaunch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addCart(Cart cart) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addCartItem(CartItem cartItem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Cart> findAllCart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Cart> findCartById(Integer cartId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<CartItem> findCartItemById(Integer itemId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<Cart> findCartsByUserId(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cart> findCartsbyUserIdAndCheckoutStatus(Integer userId, Boolean isCheckout) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Cart> findNoneCheckoutCartByUserId(Integer userId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Boolean checkoutCartByUserId(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean checkoutCartById(Integer cartId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean removeCartItemById(Integer cartItemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean updateCartItemQuantity(Integer cartItemId, Integer quantity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
